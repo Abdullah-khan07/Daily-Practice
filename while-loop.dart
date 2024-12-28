4d8912770f6657a2aad52ed10bd5df9307c6ee23
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-28 12:37:06 -0800

    email & password

---
+++ while-loop.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  bool login = false;
+  while (login == false) {
+    stdout.write("Please input the email: ");
+    var email = stdin.readLineSync()!;
+
+    stdout.write("Please input the password: ");
+    var password = stdin.readLineSync()!;
+
+    if (email == "abdullah@gmail.com" && password == "5612") {
+      // Removed the semicolon here
+      print("Login successful");
+      login = true;
+    } else {
+      print("Login failed");
+    }
+  }
+}
