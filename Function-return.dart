1106ff38e9f87385121eb32273aa5e05ec54ba67
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2025-01-05 01:54:04 -0800

    num return

--- Function-return.dart
+++ Function-return.dart
@@ -4,7 +4,7 @@ void main (){
   num abc = sumoftwonumber(value1:45, value2: 100);
   print(abc);
 }
-sumoftwonumber ({required value1 , required value2  }){
+ num sumoftwonumber ({required value1 , required value2  }){
   num result = ( value1+value2);
   return (result);
 }
