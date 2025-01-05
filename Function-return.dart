e02d3563d414385456a13549b2f8f941245e1e69
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2025-01-05 15:58:54 -0800

    Login function

--- Function-return.dart
+++ Function-return.dart
@@ -1,27 +1,23 @@
-
 import 'dart:io';
-void main (){
+
+void main() {
   bool login = loginfunction();
   print(login);
 }
 
-bool loginfunction (){
-  bool login =(false);
-  while (login== false) {
+bool loginfunction() {
+  bool login = (false);
+  while (login == false) {
     stdout.write("please input the email =");
-    String email =stdin.readLineSync()!;
-     stdout.write("please input the password =");
-     String password =stdin.readLineSync()!;
-    if (email== "abdullah" && password == "5678") {
+    String email = stdin.readLineSync()!;
+    stdout.write("please input the password =");
+    String password = stdin.readLineSync()!;
+    if (email == "abdullah" && password == "5678") {
       print("login suceesful");
       login = true;
-      
     } else {
       print("login fail");
-      
     }
   }
-  return(login);
-
+  return (login);
 }
-
