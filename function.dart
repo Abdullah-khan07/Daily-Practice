e475123b60ca119d47682220aeecc0a9f76a7fbf
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2025-01-04 18:50:45 -0800

    required parameter paractice

--- function.dart
+++ function.dart
@@ -1,17 +1,12 @@
-import 'dart:io';
-
 void main (){
-  print("parents");
-  marksheet();
-  print("Teacher");
-  marksheet();
-  print("Students");
-  marksheet();
+  marksheet(Name: "Huzaifa" ,percentage: 80);
+  marksheet(Name: "Abdullah" ,percentage: 3);
+  marksheet(Name: "Rizwan" ,percentage: 8);
 }
-marksheet (){
-  print("Name");
-  num parcentage =50;
-  if (parcentage > 50) {
+marksheet ({required String Name ,required num percentage, sec = "A"}){
+  print("Name $Name");
+  print("sec $sec");
+  if (percentage >= 50) {
     print("pass");
     
   } else {
