eb9b5ff0ec11e4eec179f851d4fc363ef006bf48
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2025-01-05 19:18:37 -0800

    Add two numbers

---
+++ Function-Paractice.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main (){
+  // Function 1: Add two numbers
+print("please enter the two number");
+  int add = addtwonumber(56, 78);
+  print(add);
+}
+int addtwonumber(int a ,int b){
+  return (a+b);
+
+}
