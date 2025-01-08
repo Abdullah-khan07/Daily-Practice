900934db5d2d4845b1b4885a90fe21968715d1f6
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2025-01-08 01:12:22 -0800

    Reverse a string

--- Function-Paractice.dart
+++ Function-Paractice.dart
@@ -1,12 +1,9 @@
 void main (){
-num numbers = Divide(100, 5);
-print(numbers); // Output: 20.0
+  /////Function 5: Reverse a string
+String name = ReverseString("Punjab");
+print(name);
+  
 }
-// Function 4: Divide two numbers
-num Divide(int a, int b) {
-  if (b == 0) {
-    throw ArgumentError("Cannot divide by zero");
-  }
-  num result = a / b;
-  return num.parse(result.toStringAsFixed(1));
+String ReverseString ( String input){
+   return input.split('').reversed.join('');
 }
